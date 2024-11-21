3459f31a627da995964ceaa56131c91e52f22154
anakovalchuk <annakovalchuk2008@gmail.com>
2024-11-21 20:39:12 +0000

    Deploying to gh-pages from @ anakovalchuk/goit-js-hw-09@f3854d707647be3addbe06a55548aa01ae019be8 🚀

---
+++ 1-gallery.js
@@ -0,0 +1,2 @@
+import"./assets/styles-CY-9B7rY.js";console.log("Gallery");
+//# sourceMappingURL=1-gallery.js.map
---
+++ 2-form.js
@@ -0,0 +1,2 @@
+import"./assets/styles-CY-9B7rY.js";console.log("Form");
+//# sourceMappingURL=2-form.js.map
